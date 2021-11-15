ea69de975d7f6e16bcb63b2f7013f373d1006e12
stephen-lakes <oluyomiolamilekan99@gmail.com>
2021-11-15 11:22:41 +0100

    added capitalize sentence function

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
-def input_processor(input_string, cap=False):
+def input_processor(input_string:str, cap=False) -> str:
     if cap:
         return input_string.strip().capitalize()
     return input_string.strip()
 
+def capitalize_sentence(sentence:str) -> str:
+    return ' '.join([word.capitalize() for word in sentence.split()])
+    
+
 def clean_input(text):
     pass
+
+
+print(capitalize_sentence("university of lagos"))
