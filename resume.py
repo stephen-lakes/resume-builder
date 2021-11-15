0b5dbecdaaeadfbedf5e54af2fe730ce6ed6417f
stephen-lakes <oluyomiolamilekan99@gmail.com>
2021-11-15 12:17:14 +0100

    modified resume.py

--- resume.py
+++ resume.py
@@ -8,11 +8,11 @@ print("\/\/\/\/======== Professioanl Resume Builder ========\/\/\/\/\n")
 print("\t\t\tPERSONAL INFO\n")
 print("Lets Fill in your personal details\n")
 personal_info = {}
-resume_role         = input_processor(input("role> "), True)
-first_name          = input_processor(input("first_name> "), True)
-last_name           = input_processor(input("last_name> "), True)
-email               = input_processor(input("email> "))
-phone               = input_processor(input("phone> "))
+resume_role         = input_processor(input("\trole> "), True)
+first_name          = input_processor(input("\tfirst_name> "), True)
+last_name           = input_processor(input("\tlast_name> "), True)
+email               = input_processor(input("\temail> "))
+phone               = input_processor(input("\tphone> "))
 
 personal_info["role"]       = resume_role
 personal_info["first_name"] = first_name
@@ -28,30 +28,30 @@ experiences = []
 more_position = True
 while more_position:
 
-    job_role     = input_processor(input("Job Title> "), True)
-    city         = input_processor(input("City> "), True)
-    company      = input_processor(input("Employer/Company> "), True)
+    job_role     = input_processor(input("\tJob Title> "), True)
+    city         = input_processor(input("\tCity> "), True)
+    company      = input_processor(input("\tEmployer/Company> "), True)
     print("When did you start this job?")
-    start_date   = input_processor(input("Hint: jan 2020> "), True)
+    start_date   = input_processor(input("\tHint: jan 2020> "), True)
     
-    works_here   = input("Do work here currently(y/n)> ").lower()
+    works_here   = input("\tDo work here currently(y/n)> ").lower()
     if works_here in ("yes", "y", ""):
         end_date = "Current"
     else:
         print("When did you quit this job")
-        end_date = input("Hint: dec 2020> ")
+        end_date = input("\tHint: dec 2020> ")
         print("")
 
-    print("Nice! Now let's describe what you did \n\t Type 'd' when you are done with the accomplishments")
+    print("\nNice! Now let's describe what you did \n Type 'd' when you are done with the accomplishments")
     tasks = []
     while True:
-        task = input("Hint: Conducted tests of components and systems to evaluate performance and identify concerns.\n\t>")
+        task = input("\tHint: Conducted tests of components and systems to evaluate performance and identify concerns.\n\t>")
         if task.lower() in ("done", "do", "d"):
             break
         tasks.append(task)
         
 
-    add_more_position = input_processor(input("Do you want to add another position(y/n)> ")).lower()
+    add_more_position = input_processor(input("\tDo you want to add another position(y/n)> ")).lower()
     if add_more_position in ("no", "n"):
         more_position = False
 
@@ -75,22 +75,22 @@ education = []
 edu_history = True
 while edu_history:
 
-    course       = input_processor(input("Course> "), True)
-    location     = input_processor(input("Location> "), True)
-    institution  = capitalize_sentence(input_processor(input("School/Institution> "), True))
+    course       = input_processor(input("\tCourse> "), True)
+    location     = input_processor(input("\tLocation> "), True)
+    institution  = capitalize_sentence(input_processor(input("\tSchool/Institution> "), True))
     print("When did you start?")
-    start_date   = input_processor(input("Hint: jan 2020> "), True)
+    start_date   = input_processor(input("\tHint: jan 2020> "), True)
     
-    works_here   = input("Do you school here currently(y/n)> ").lower()
+    works_here   = input("\tDo you school here currently(y/n)> ").lower()
     if works_here in ("yes", "y", ""):
         end_date = "Current"
     else:
         print("When did you graduate")
-        end_date = input("Hint: dec 2020> ")
+        end_date = input("\tHint: dec 2020> ")
         print("")
   
 
-    add_institution = input_processor(input("Do you want to add another institution(y/n)> ")).lower()
+    add_institution = input_processor(input("\tDo you want to add another institution(y/n)> ")).lower()
     if add_institution in ("no", "n"):
         edu_history = False
 
@@ -105,3 +105,14 @@ while edu_history:
     )
 
 print(education)
+
+
+print("\t\t\tTECHNICAL SKILLS\n")
+print("\nLets Fill in your technical skills\n\t Type 'd' when you are done with this section.")
+technical_skils = []
+while True:
+        command = input("\tHint: Python, MySQL, PHP, scrum, Agile, AWS.\n\t>")
+        if command.lower() == "d" or command.lower() == "done":
+            break
+        else:
+            technical_skils.append(command)
